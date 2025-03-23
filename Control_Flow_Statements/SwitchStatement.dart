1507bf8b1305245e198088aa5332e20f6a86565b
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-23 18:32:43 +0530

    Switch Statement

---
+++ Control_Flow_Statements/SwitchStatement.dart
@@ -0,0 +1,17 @@
+void main() {
+  var x = "Amrut";
+  switch (x) {
+    case 1:
+      print("The value is equal to zero");
+      break;
+    case 2:
+      print("The value is ;ess than 15");
+      break;
+    case 3:
+      print("The value is equal to 15");
+      break;
+    default:
+      print("Some Other value");
+      break;
+  }
+}
