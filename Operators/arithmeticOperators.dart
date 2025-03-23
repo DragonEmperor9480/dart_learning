e6aa8dca439d9941a69a1265cfe53bafc1c321cf
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-23 16:04:33 +0530

    Arithmetic Operators

---
+++ Operators/arithmeticOperators.dart
@@ -0,0 +1,8 @@
+void main() {
+  print(12 + 12); //Addition
+  print(12 - 12); //Subtraction
+  print(12 * 12); //Multiplication
+  print(57 / 6); //Division
+  print(57 ~/ 6); //Integer division
+  print(57 % 6); //Modulus
+}
