9df0507dac69928d84f525d2aa78ccfc135ed118
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-23 18:29:30 +0530

    IfElseIf Statement

---
+++ Control_Flow_Statements/IfElseIfStatements.dart
@@ -0,0 +1,11 @@
+void main() {
+  int x = 15;
+  if (x == 0) {
+    print("The value is equal to 0");
+  } else if (x < 15)
+    print("The value is less than 15");
+  else if (x == 15)
+    print("The Value is equal to 15");
+  else
+    print("The value is greater than 15");
+}
