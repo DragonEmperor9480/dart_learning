678b2580066deacd5a0e53ebc25de35e2650153f
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-23 16:09:27 +0530

    Comparision Operators

---
+++ Operators/ComparisionOperators.dart
@@ -0,0 +1,15 @@
+void main() {
+  // == Equals to Operator
+  print(5 == 5);
+  // != Not Equals Operator
+  print(5 != 4);
+  // > Greater than
+  print(5 > 4);
+  // < Less than
+  print(5 < 4);
+  // >= Greater than equal to
+  print(5 >= 5);
+  // <= Less than equal to
+  print(5 <= 5);
+  
+}
