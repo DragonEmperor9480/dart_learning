0550359395694a56ee05ec2eb98c6b8eee2c31db
DragonEmperor9480 <amruteshnaregal1234@gmail.com>
2025-03-23 16:30:16 +0530

    Assignment Operators

---
+++ Operators/AssignmentOperators.dart
@@ -0,0 +1,13 @@
+void main() {
+  int x = 5; //assign
+  x += 5; // Add and Assign
+  x -= 5; // Subtract and assign
+  x *= 5; //multiply and assign
+  /*
+  *
+  *
+  *
+  */
+  x ??= 10; // Assign 10 if null
+  print(x);
+}
